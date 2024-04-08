b75428a02bd008d2ff63eb5bf987dbf381a38490
lyrahgames <lyrahgames@mailbox.org>
2024-04-08 20:37:59 +0200

    Allow executable packages to have 'lib' prefix

--- default-options/assert-ensketch-prefix.sh
+++ default-options/assert-ensketch-prefix.sh
@@ -9,10 +9,19 @@ if [[ "$BDEP_NEW_TYPE" == "library" ]]; then
   echo ""
 elif [[ "$BDEP_NEW_TYPE" == "executable" ]]; then
   [[ $BDEP_NEW_NAME == ensketch-* ]] && exit
+  if [[ $BDEP_NEW_NAME == libensketch-* ]]; then
+    echo "WARNING: Executable package name starts with 'lib'!"
+    echo ""
+    echo "Please make sure that '$BDEP_NEW_NAME' is used"
+    echo "to test parts of a library package that is"
+    echo "contained in this package repository."
+    echo ""
+    exit
+  fi
   echo "ERROR: Failed to create new build2 package!"
   echo ""
   echo "The given package name '$BDEP_NEW_NAME'"
-  echo "does not start with 'ensketch-'."
+  echo "does not start with 'ensketch-' or 'libensketch-'."
   echo ""
 else
   exit
